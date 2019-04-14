c06e616801317efa8881f5864cd5690ee7d58c8f
Lucas <lucasrm25@gmail.com>
2019-04-14 17:03:22 +0200

    All the plots needed for the analysis report finished.

--- HA02/main.m
+++ HA02/main.m
@@ -24,7 +24,7 @@ Y = genLinearMeasurementSequence(X, H, R);
 
 clr = fp.getColor(1:6);
 
-figure('Color','white','Position',[591  304  560  420]);
+figure('Color','white','Position',[586  360  505  359]);
 hold on, grid on;
 plot(0:N,X, 'b', 'DisplayName','true state sequence');
 plot(1:N,Y,'*r', 'DisplayName','measurement sequence');
@@ -52,7 +52,7 @@ p4 = plot([0:N], [x_0 x_k_k] + 3*sqrt([P_0 P_k_k(:)']), '--b', 'DisplayName','+3
 p5 = plot([0:N], [x_0 x_k_k] - 3*sqrt([P_0 P_k_k(:)']), '--b', 'DisplayName','-3-sigma level');
 xlabel('k - time step');
 ylabel('x');
-legend([p1 p2 p3 p4 p5],'Location','southeast');
+legend([p1 p2 p3 p4 p5],'Location','northeast');
 % fp.savefig('q1b')
 
 
@@ -72,7 +72,8 @@ for i=1:numel(idx)
     p2 = plot([xtrue,xtrue], [0,max(y)*1.1],'--', 'LineWidth',3, 'Color', [clr(2,:) 0.5],'DisplayName',sprintf('true state x_{%d}',idx(i)));
     
     ylim([0,max(y)*1.1])
-    title(['posterior distribution: ' name]), ylabel(name), xlabel 'state x', grid on;
+    title(['posterior distribution: $' name,'$'], 'Interpreter','Latex');
+    ylabel(name), xlabel 'state x';
     legend('Location','southeast')
 %     fp.savefig(sprintf('q1b_%d',idx(i)))
 end
@@ -128,7 +129,8 @@ Y = genLinearMeasurementSequence(X, H, R);
 [x_k_k, P_k_k, x_k_km1, P_k_km1, v, S, K] = kalmanFilter2(Y, x_0, P_0, A, Q, H, R);
 
 
-xmean = mean(X)
+mean(x_k_k)
+mean(v)
 
 figure('Color','white','Position',[704  497  589  276]);
 hold on, grid on;
@@ -148,7 +150,7 @@ legend('Interpreter','Latex')
 figure('Color','white','Position',[704  497  589  276]);
 hold on, grid on;
 autocorr(v)
-xlabel 'Lag', ylabel 'Sample autocorrelation', title 'Sample utocorrelation function of innovation'
+xlabel 'Lag', ylabel 'Sample autocorrelation', title 'Sample autocorrelation function of innovation'
 % fp.savefig(sprintf('q1d-aut'))
 
 
@@ -172,7 +174,7 @@ P_0w = 6;
 
 
 % plot results
-figure('Color','white','Position',[575  274  560  420]);
+figure('Color','white','Position',[565  353  656  331]);
 hold on, grid on;
 
 p1 = plot([0:N], [x_0 x_k_k], 'Color',[clr(1,:),0.5], 'LineWidth',3, 'DisplayName','state estimate');
@@ -183,11 +185,13 @@ p4 = plot([0:N], [x_0w x_k_kw], 'Color',[clr(3,:),0.5], 'LineWidth',3, 'DisplayN
 p5 = plot([0:N], [x_0w x_k_kw] + 3*sqrt([P_0w P_k_kw(:)']), '--', 'Color',clr(3,:), 'LineWidth',2, 'DisplayName','+3-sigma level');
 p6 = plot([0:N], [x_0w x_k_kw] - 3*sqrt([P_0w P_k_kw(:)']), '--', 'Color',clr(3,:), 'LineWidth',2, 'DisplayName','-3-sigma level');
 
+p8 = plot([0:N], X, '-', 'Color',[0 0 0 1], 'LineWidth',1, 'DisplayName','true state');
+
 p7 = plot([1:N], Y, '*r', 'DisplayName','measurements');
 
 xlabel('k - time step');
 ylabel('x');
-legend([p1 p2 p3 p4 p5 p6 p7],'Location','southeast');
+legend([p1 p2 p3 p4 p5 p6 p8 p7],'Location','southeast');
 % fp.savefig('q1e')
 
 
@@ -196,7 +200,7 @@ legend([p1 p2 p3 p4 p5 p6 p7],'Location','southeast');
 
 close all;
 
-T = 0.1;
+T = 0.01;
 
 % Motion Parameters
 A= [1 T; 0 1];
@@ -210,7 +214,7 @@ x_0 = [1;3];
 P_0 = 4*eye(2);
 
 % General parameters
-N = 20;
+N = 50;
 
 % calculate state and measurement sequences
 X = genLinearStateSequence(x_0,P_0,A,Q,N);
@@ -218,21 +222,21 @@ Y = genLinearMeasurementSequence(X, H, R);
 
 
 % plot position
-figure('Color','white','Position',[385  444  437  294]);
+figure('Color','white','Position',[340  124  427  294]);
 hold on, grid on;
 plot(0:N,H*X, 'b', 'DisplayName','true state sequence');
 plot(1:N,Y,'*r', 'DisplayName','measurement sequence');
 xlabel 'k - time step', ylabel 'position'
-legend
+legend('Location','northwest')
 % fp.savefig('q2a-pos')
 
 
 % plot velocity
-figure('Color','white','Position',[832  448  437  294]);
+figure('Color','white','Position',[781  127  447  294]);
 hold on, grid on;
 plot(0:N,X(2,:), 'b', 'DisplayName','velocity sequence');
 xlabel 'k - time step', ylabel 'velocity'
-legend
+legend('Location','northwest')
 % fp.savefig('q2a-vel')
 
 
@@ -244,7 +248,7 @@ close all;
 [x_k_k, P_k_k, x_k_km1, P_k_km1, v, S, K] = kalmanFilter2(Y, x_0, P_0, A, Q, H, R);
 
 % plot position
-figure('Color','white','Position',[575  274  560  420]);
+figure('Color','white','Position',[321  517  457  294]);
 hold on, grid on;
 p3 = plot(0:N, H*X, 'b', 'LineWidth',3, 'DisplayName','true state');
 p3.Color = [p3.Color 0.2];
@@ -254,21 +258,22 @@ p4 = plot(0:N, H*[x_0 x_k_k] + 3*sqrt([P_0(1) squeeze(P_k_k(1,1,:))']), '--b', '
 p5 = plot(0:N, H*[x_0 x_k_k] - 3*sqrt([P_0(1) squeeze(P_k_k(1,1,:))']), '--b', 'DisplayName','-3-sigma level');
 xlabel('k - time step');
 ylabel('position');
-legend([p1 p2 p3 p4 p5],'Location','southeast');
+legend([p1 p2 p3 p4 p5],'Location','northwest'); ylim([-3 11])
 % fp.savefig('q2b-pos')
 
 
+Hv = [0 1];
 % plot velocity
-figure('Color','white','Position',[575  274  560  420]);
+figure('Color','white','Position',[788  517  447  294]);
 hold on, grid on;
-p3 = plot(0:N, H*X, 'b', 'LineWidth',3, 'DisplayName','true state');
+p3 = plot(0:N, Hv*X, 'b', 'LineWidth',3, 'DisplayName','true state');
 p3.Color = [p3.Color 0.2];
-p2 = plot(0:N, H*[x_0 x_k_k], 'b', 'LineWidth',1.5, 'DisplayName','state estimate');
-p4 = plot(0:N, H*[x_0 x_k_k] + 3*sqrt([P_0(1) squeeze(P_k_k(1,1,:))']), '--b', 'DisplayName','+3-sigma level');
-p5 = plot(0:N, H*[x_0 x_k_k] - 3*sqrt([P_0(1) squeeze(P_k_k(1,1,:))']), '--b', 'DisplayName','-3-sigma level');
+p2 = plot(0:N, Hv*[x_0 x_k_k], 'b', 'LineWidth',1.5, 'DisplayName','state estimate');
+p4 = plot(0:N, Hv*[x_0 x_k_k] + 3*sqrt([P_0(2,2) squeeze(P_k_k(2,2,:))']), '--b', 'DisplayName','+3-sigma level');
+p5 = plot(0:N, Hv*[x_0 x_k_k] - 3*sqrt([P_0(2,2) squeeze(P_k_k(2,2,:))']), '--b', 'DisplayName','-3-sigma level');
 xlabel('k - time step');
-ylabel('position');
-legend([p2 p3 p4 p5],'Location','southeast');
+ylabel('velocity');
+legend([p2 p3 p4 p5],'Location','northwest'); ylim([-15 35])
 % fp.savefig('q2b-vel')
 
 
@@ -283,7 +288,7 @@ close all;
 Qi = [0.1 1 10 1.5];
 
 % General parameters
-N = 30;
+N = 50;
 % calculate state and measurement sequences
 X = genLinearStateSequence(x_0,P_0,A,Q,N);
 Y = genLinearMeasurementSequence(X, H, R);
@@ -293,7 +298,7 @@ for i=1:numel(Qi)
     [x_k_k, P_k_k, x_k_km1, P_k_km1, v, S, K] = kalmanFilter2(Y, x_0, P_0, A, tao*Qi(i)*tao', H, R);
     
     % plot position
-    figure('Color','white','Position',[575  274  560  420]);
+    figure('Color','white','Position',[192  538  421  303]);
     hold on, grid on;
     p3 = plot(0:N, H*X, 'b', 'LineWidth',3, 'DisplayName','true state');
     p3.Color = [p3.Color 0.2];
@@ -305,9 +310,8 @@ for i=1:numel(Qi)
     ylabel('position');
     title(sprintf('Kalman filter, Q=%.1f',Qi(i)))
     legend([p1 p2 p3 p4 p5],'Location','southeast');
-    % fp.savefig(sprintf('q2c-pos-Qi-%d',Qi(i))
+%     fp.savefig(sprintf('q2c-pos-Qi-%d',i));
 end
-
 
 
 
